bde6d3eee961106614952690763eae83463a6af2
Anna Marie Sterling <sterling.am@gmail.com>
2022-04-21 19:58:17 -0600

    Completed pairing 3

--- friends.rb
+++ friends.rb
@@ -1,6 +1,12 @@
 numbers = [2, 4, 6, 8, 10, 12, 14, 36]
 numbers.each do |number|
-  if number >= 30
+  if number > 30
+    puts "#{number}"
+  end
+end
+
+numbers.each do |number|
+  if number < 60
     puts "#{number}"
   end
 end
